09753ee926123ee1d8aceb99e7c55d096843b5ad
aaronthom <hampeaaron@gmail.com>
2024-02-04 05:11:29 +0100

    Konfigurator- erste reihe gebaut

---
+++ app.vue
@@ -0,0 +1,23 @@
+<!--konfigurator: 
+
+-zwei main container -> zeilen container und spalten container
+- zwei main objekte -> Zeile und Spalte
+
+-->
+
+
+
+
+
+<script setup lang="ts">
+import RowObject from './components/RowObject.vue';
+
+</script>
+  
+ 
+ 
+<template>
+  <RowObject></RowObject>
+</template>
+ 
+ 
